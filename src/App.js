5166f5e17b2362f8ef19f0704d42a70c9a84951d
murashko12 <pert@ocumare.ru>
2022-11-24 18:34:07 +0300

    weather app react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from "react";
+import axios from "axios";
 function App() {
+  const [data,setData] = useState({})
+  const [location,setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5fdf2f14de8b99b41be976e257984a18`;
+
+
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data);
+      })
+      setLocation('');
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text"/>
+      </div>
+      <div className="container">
+        <div className="top">
+
+          <div className="location"> {/* локация (город) */}
+
+            <p>{data.name}</p>
+          </div>
+
+          <div className="temp"> {/* температура */}
+            {data.main ? <h1>{Math.round(data.main.temp - 273)} °C</h1> : null}
+          </div>
+
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name != undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className="bold">{Math.round(data.main.feels_like - 273)} °C</p> : "___"}
+              <p>Feels like</p>
+            </div>
+
+            <div className="humibity"> {/* влажность */}
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : "___"}
+              {/* <p className="bold">85%</p>  */}
+              <p>Humibity</p> 
+            </div>
+            <div className="wind">
+              {data.wind ? <p className="bold">{(data.wind.speed * 1000 / 3600).toFixed(2)} m/s</p> : "___"}
+              {/* <p className="bold">2 m/s</p> */}
+              <p>Wind Speed</p> 
+            </div> 
+          </div>
+        }
+
+
+      </div>
     </div>
   );
 }
